b07467461ec23252f8df6fbbec88f29afc061fe3
CDinuwan <chanukadinuwan35@gmail.com>
2020-04-19 16:53:56 +0530

    Updated header and categories list

---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,34 @@
+import '../models/category_model.dart';
+
+final categories=[
+  Category(
+    numberOfItems:43,
+    imagePath: "asserts/friedrice.png",
+    categoryName: "Fried Rice"
+  ),
+  Category(
+    numberOfItems:23,
+    imagePath: "asserts/pizza.png",
+    categoryName: "Pizza"
+  ),
+  Category(
+    numberOfItems:13,
+    imagePath: "asserts/cake.png",
+    categoryName: "Cake"
+  ),
+  Category(
+    numberOfItems:83,
+    imagePath: "asserts/coffee.png",
+    categoryName: "Coffee"
+  ),
+  Category(
+    numberOfItems:23,
+    imagePath: "asserts/cooldrinks.png",
+    categoryName: "Soft Drinks"
+  ),
+  Category(
+    numberOfItems:23,
+    imagePath: "asserts/lunch.png",
+    categoryName: "Rice and Curry"
+  ),
+];
--- lib/src/homescreen.dart
+++ lib/src/homescreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/src/widgets/food_category.dart';
+import 'widgets/home_top_info.dart';
+import 'widgets/food_card.dart';
 
 class HomeScreen extends StatefulWidget{
   @override
@@ -8,7 +11,7 @@ class HomeScreen extends StatefulWidget{
 class _HomeScreenState extends State<HomeScreen>
 {
 
-  final textStyle=TextStyle(fontSize:40.0,fontWeight:FontWeight.bold,);
+  
 @override
 Widget build(BuildContext context)
 {
@@ -17,23 +20,11 @@ Widget build(BuildContext context)
       padding: EdgeInsets.only(top:50.0,left: 20.0,right: 20.0),
       children:<Widget>
       [
-        Row
-        (
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("What would",style: textStyle),
-              Text("you like to eat?",style: textStyle,)
-            ]
-            ),
-            Icon(Icons.notifications_none,size: 30.0,color: Theme.of(context).primaryColor,),
-          ],
-        )
-      ]
-    ),
+        HomeTopInfo(),
+        FoodCategory(),
+        ],
+        ),
   );
+      
 }
 }
